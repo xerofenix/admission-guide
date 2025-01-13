9e21e9cf6807ab14146db3cc70a9ffbae283e335
xerofenix <xerofenix@duck.com>
2025-01-13 20:50:25 +0530

    add basic route dir and file

---
+++ go-back/routes/rotutes.go
@@ -0,0 +1,9 @@
+package routes
+
+import "github.com/gofiber/fiber/v2"
+
+func SetUp(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).SendString("hello world. This page shows that the wbesite is working fine and the server is currently running")
+	})
+}
