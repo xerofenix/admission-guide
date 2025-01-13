caf3d3c3d0daf9ce89cc23ccf03bf248756bb433
xerofenix <xerofenix@duck.com>
2025-01-13 22:08:01 +0530

    add func for loading env and connecting db

--- go-back/main.go
+++ go-back/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"admission-guide/config"
 	"admission-guide/routes"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	// load .env file
+	config.LoadEnv()
+
+	//connecting to DB
+	config.ConnectDB()
 
 	app := fiber.New()
 	routes.SetUp(app)
