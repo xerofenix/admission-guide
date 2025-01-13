fcc5a252184070c60d08acb903a398ec528da046
xerofenix <xerofenix@duck.com>
2025-01-13 21:54:59 +0530

    add load env file

---
+++ go-back/config/loadEnv.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
+// loading .env file
+func LoadEnv() {
+
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error! loading env", err)
+	}
+}
