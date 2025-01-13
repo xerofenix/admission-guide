c95b244196ca37e5bcbf7cd0aae3899c3a1bd068
xerofenix <xerofenix@duck.com>
2025-01-14 00:27:01 +0530

    add routes with some changes

--- go-back/routes/rotutes.go
+++ go-back/routes/rotutes.go
@@ -1,9 +1,14 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"admission-guide/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func SetUp(app *fiber.App) {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).SendString("hello world. This page shows that the wbesite is working fine and the server is currently running")
 	})
+	app.Post("/feedback", controllers.SubmitFeedback)
 }
